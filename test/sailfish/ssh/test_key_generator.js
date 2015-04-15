effe53d3e594b6860ee1167553435241514d7bf1
Albert Lacarta <urodoz@gmail.com>
2015-04-15 14:26:58 +0200

    Added keypair generator class, with configurable bits opt

---
+++ test/sailfish/ssh/test_key_generator.js
@@ -0,0 +1,33 @@
+var assert = require("assert");
+var keyGeneratorFactory = require("sailfish/ssh/key_generator");
+var _ = require("underscore");
+var S = require("string");
+
+/**
+ * @code
+ * NODE_PATH=$NODE_PATH:./lib node_modules/.bin/mocha --recursive test/sailfish/ssh/test_key_generator.js
+ * @endcode
+ */
+describe("ssh key_generator", function() {
+
+    this.timeout(3000);
+
+    it('simple check of the keypair generated', function(done) {
+        var keyGenerator = new keyGeneratorFactory(1024);
+        var keyPair = keyGenerator.generate();
+
+        assert.ok(_.has(keyPair, "public"));
+        assert.ok(_.has(keyPair, "private"));
+        assert.ok(_.isString(keyPair["public"]));
+        assert.ok(_.isString(keyPair["private"]));
+
+        //String content assertions
+        assert.ok(S(keyPair["public"]).startsWith("-----BEGIN RSA PUBLIC KEY-----"));
+        assert.ok(S(keyPair["private"]).startsWith("-----BEGIN RSA PRIVATE KEY-----"));
+        assert.ok(S(keyPair["public"]).contains('-----END RSA PUBLIC KEY-----'));
+        assert.ok(S(keyPair["private"]).contains('-----END RSA PRIVATE KEY-----'));
+
+        done();
+    });
+
+});
